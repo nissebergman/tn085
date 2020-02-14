60b71ad63ed09fa327ccc7406b37f6937f983901
nibe91 <nibe91@gmail.com>
2020-02-14 18:27:55 +0100

    ye

--- index.php
+++ index.php
@@ -3,16 +3,16 @@
 
 <head>
     <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
-    <script src='./WebGL/JS/three.min.js'></script>
-    <script src='./WebGL/JS/OrbitControls.js'></script>
-    <script src='./WebGL/JS/GLTFLoader.js'></script>
-    <script src='./WebGL/Extern/noisejs/perlin.js'></script>
-    <link rel="stylesheet" href="WebGL/style/style.css">
+    <script src='/WebGL/JS/three.min.js'></script>
+    <script src='/WebGL/JS/OrbitControls.js'></script>
+    <script src='/WebGL/JS/GLTFLoader.js'></script>
+    <script src='/WebGL/Extern/noisejs/perlin.js'></script>
+    <link rel="stylesheet" href="/WebGL/style/style.css">
 </head>
 
 <body>
 
-    <script src="JS/main.js"></script>
+    <script src="/WebGL/JS/main.js"></script>
 </body>
 
 </html>
