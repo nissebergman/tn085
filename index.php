cef07a11098b7fde8e8e8aea323776dd9def827f
nibe91 <nibe91@gmail.com>
2020-02-14 18:23:23 +0100

    ye

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php include_once("WebGL/index.html");?>
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+    <script src='WebGL/JS/three.min.js'></script>
+    <script src='WebGL/JS/OrbitControls.js'></script>
+    <script src='WebGL/JS/GLTFLoader.js'></script>
+    <script src='WebGL/Extern/noisejs/perlin.js'></script>
+    <link rel="stylesheet" href="WebGL/style/style.css">
+</head>
+
+<body>
+    <script src="JS/main.js"></script>
+</body>
+
+</html>
